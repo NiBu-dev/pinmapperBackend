c04096ba0e22b64dafde0c3fad149e5ac957d2fc
Nick <sahas023@gmail.com>
2020-01-18 12:30:35 +0100

    initial commit

---
+++ build/precache-manifest.952b6aa3cdcbbcb640ad096c921c8d35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ea1b999a4be58abc6c7e39a8018f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34aea208a9e13a529693",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "39419fb5f2d79cc21ff6",
+    "url": "/static/css/main.3883b19f.chunk.css"
+  },
+  {
+    "revision": "34aea208a9e13a529693",
+    "url": "/static/js/2.2618de54.chunk.js"
+  },
+  {
+    "revision": "39419fb5f2d79cc21ff6",
+    "url": "/static/js/main.14ce89e4.chunk.js"
+  },
+  {
+    "revision": "5492306b4319c1e69fe8",
+    "url": "/static/js/runtime-main.82eb0456.js"
+  },
+  {
+    "revision": "4591a3adfa9053eb4d93b8d19c2a2432",
+    "url": "/static/media/277.4591a3ad.svg"
+  },
+  {
+    "revision": "6fcf16d24ffcc82c0745b6bd5653497f",
+    "url": "/static/media/logo2.6fcf16d2.svg"
+  },
+  {
+    "revision": "63e697748ceea0a2d18a56cab7e5ba31",
+    "url": "/static/media/main.63e69774.jpg"
+  },
+  {
+    "revision": "bf5e8ca48065f0abf93e02ad02471962",
+    "url": "/static/media/reaper.bf5e8ca4.svg"
+  },
+  {
+    "revision": "75851b68372d5b1456cf527a80029271",
+    "url": "/static/media/smartphone.75851b68.svg"
+  }
+]);
