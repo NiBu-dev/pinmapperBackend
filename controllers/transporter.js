cbd144544fd75eaef44d54e5a641af62cc737e9f
Nick <sahas023@gmail.com>
2020-01-19 11:04:11 +0100

    testing with only dev setup

--- controllers/transporter.js
+++ controllers/transporter.js
@@ -1,25 +1,12 @@
 var nodemailer = require('nodemailer');
 
 var transport;
-if (process.env.NODE_ENV === 'production') {
-    transport = {
-        host: 'smtp.gmail.com',
-        auth: {
-            user: process.env.MAIL_ADDR,
-            pass: process.env.MAIL_PASS
-        }
+transport = {
+    host: 'smtp.gmail.com',
+    auth: {
+        user: process.env.MAIL_ADDR,
+        pass: process.env.MAIL_PASS
     }
-} else if (process.env.NODE_ENV === 'development') {
-    const creds = require('../mailConfig');
-
-    transport = {
-        host: 'smtp.gmail.com',
-        auth: {
-            user: creds.USER,
-            pass: creds.PASS
-        }
-    }
-    
 }
 console.log(process.env.NODE_ENV)
 
@@ -28,11 +15,11 @@ console.log(process.env.NODE_ENV)
 var transporter = nodemailer.createTransport(transport)
 
 transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is ready to take messages');
-    }
-});
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Server is ready to take messages');
+        }
+    });
 
-module.exports = transporter;
+    module.exports = transporter;
