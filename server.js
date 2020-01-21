75612806ca187f3ea4e61912d70413bd29588cea
Nick <sahas023@gmail.com>
2020-01-21 23:04:25 +0100

    added db url to env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: true, credentials: true }));
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_STRING, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('connected to database'));
